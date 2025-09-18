1063ef0d04fd99ac9c26a1d2450c4add21da57c3
farhadham <farhadham2@gmail.com>
2025-09-18 13:35:39 +0200

    increased limit to 10000

--- apps/backend/src/routes/user/validators.ts
+++ apps/backend/src/routes/user/validators.ts
@@ -2,15 +2,15 @@ import { z } from "zod";
 
 export const updateUserRequestSchema = z.object({
 	name: z.string().min(1),
-	resumeContent1: z.string().min(1500).max(5000),
+	resumeContent1: z.string().min(1500).max(10000),
 	resumeContent2: z
 		.string()
-		.max(5000)
+		.max(10000)
 		.or(z.literal(""))
 		.transform((val) => val || null),
 	resumeContent3: z
 		.string()
-		.max(5000)
+		.max(10000)
 		.or(z.literal(""))
 		.transform((val) => val || null),
 	geminiKey: z.string().min(10),
